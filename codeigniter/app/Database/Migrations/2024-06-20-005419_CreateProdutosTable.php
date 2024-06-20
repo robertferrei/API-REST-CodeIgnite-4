1b5db082fb1911b08cc3c1e2e781c0194100e45c
robertferrei <ferreirarobert727@gmail.com>
2024-06-19 22:13:38 -0300

    Criando migrations e definindo banco de dados e definindo tabelas

---
+++ codeigniter/app/Database/Migrations/2024-06-20-005419_CreateProdutosTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' =>5,
+                'unsigned' => true,
+                'auto_increment' => true,
+
+            ],
+            'produto' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'valor' => [
+                'type' => 'DOUBLE',
+                
+            ]
+
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('produtos');
+        //
+    }
+
+    public function down()
+    {
+     $this->forge->dropTable('produtos');
+        //
+    }
+}
